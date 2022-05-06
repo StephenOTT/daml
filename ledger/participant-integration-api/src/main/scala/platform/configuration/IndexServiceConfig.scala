1f8666f68188f916c64a0f44d2894c4fc2c18442
Sergey Kisel <sergey.kisel@digitalasset.com>
2022-05-06 16:00:09 +0200

    address code review concerns

---
+++ ledger/participant-integration-api/src/main/scala/platform/configuration/IndexServiceConfig.scala
@@ -0,0 +1,40 @@
+// Copyright (c) 2022 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.daml.platform.configuration
+
+import java.io.File
+
+import scala.concurrent.duration.{Duration, FiniteDuration}
+
+final case class IndexServiceConfig(
+    archiveFiles: List[File] = IndexServiceConfig.DefaultArchiveFiles,
+    eventsPageSize: Int = IndexServiceConfig.DefaultEventsPageSize,
+    eventsProcessingParallelism: Int = IndexServiceConfig.DefaultEventsProcessingParallelism,
+    acsIdPageSize: Int = IndexServiceConfig.DefaultAcsIdPageSize,
+    acsIdFetchingParallelism: Int = IndexServiceConfig.DefaultAcsIdFetchingParallelism,
+    acsContractFetchingParallelism: Int = IndexServiceConfig.DefaultAcsContractFetchingParallelism,
+    acsGlobalParallelism: Int = IndexServiceConfig.DefaultAcsGlobalParallelism,
+    maxContractStateCacheSize: Long = IndexServiceConfig.DefaultMaxContractStateCacheSize,
+    maxContractKeyStateCacheSize: Long = IndexServiceConfig.DefaultMaxContractKeyStateCacheSize,
+    maxTransactionsInMemoryFanOutBufferSize: Long =
+      IndexServiceConfig.DefaultMaxTransactionsInMemoryFanOutBufferSize,
+    enableInMemoryFanOutForLedgerApi: Boolean =
+      IndexServiceConfig.DefaultEnableInMemoryFanOutForLedgerApi,
+    apiStreamShutdownTimeout: Duration = IndexServiceConfig.DefaultApiStreamShutdownTimeout,
+)
+
+object IndexServiceConfig {
+  val DefaultEventsPageSize: Int = 1000
+  val DefaultEventsProcessingParallelism: Int = 8
+  val DefaultAcsIdPageSize: Int = 20000
+  val DefaultAcsIdFetchingParallelism: Int = 2
+  val DefaultAcsContractFetchingParallelism: Int = 2
+  val DefaultAcsGlobalParallelism: Int = 10
+  val DefaultMaxContractStateCacheSize: Long = 100000L
+  val DefaultMaxContractKeyStateCacheSize: Long = 100000L
+  val DefaultMaxTransactionsInMemoryFanOutBufferSize: Long = 10000L
+  val DefaultEnableInMemoryFanOutForLedgerApi = false
+  val DefaultArchiveFiles = List.empty[File]
+  val DefaultApiStreamShutdownTimeout: Duration = FiniteDuration(5, "seconds")
+}
